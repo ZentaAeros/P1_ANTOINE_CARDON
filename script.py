c26ca4983e8a890292212a5a65e2ea894ae387a7
Antoine Cardon <c.antoine97@gmail.com>
2022-06-22 00:07:11 +0200

    Ajout de la fonction de récupération de toutes les pages d'une catégorie

--- script.py
+++ script.py
@@ -25,3 +25,25 @@ def bookInfos(url):
     book_description = [replaceDescription, link, universalProductCode, title, priceIncludingTaxe, priceExcludingTaxe, numberAvailable, category, reviewRating, sourceImage]
 
     return book_description
+
+""" Récupère toutes les pages d'une catégorie """
+def checkPageNumber(url):
+    nouveauLien = url
+    page = requests.get(nouveauLien)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    pageCourante = 'index.html'
+    verif = True
+    x = 1
+    liens = []
+    while verif == True:
+        x += 1
+        page = requests.get(nouveauLien)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        if(page.status_code == 200):
+            liens.append(nouveauLien)
+            numeroPage = 'page-{}.html'.format(x)
+            nouveauLien = nouveauLien.replace(pageCourante, numeroPage)
+            pageCourante = numeroPage
+        else:
+            verif = False
+    return liens
